97a85d8340440a8ae386f0da67c18af0f5adcda3
Gijs Nijholt <gijs.nijholt@gmail.com>
2018-05-16 11:26:25 +0200

    Icons now shared through app and updated styling of the svgs

---
+++ src/components/Icons.js
@@ -0,0 +1,48 @@
+import React from "react";
+export const IconActiveAlarmSVG = 
+   <svg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+    <g id='Page-1' fill='none' fillRule='evenodd'>
+        <g id='iconactivealarm'>
+            <g id='Shape' fill='#FF001B' fillRule='nonzero'>
+                <g id='path0_fill-link'>
+                    <path d='M10,0 C4.48,0 0,4.48 0,10 C0,15.52 4.48,20 10,20 C15.52,20 20,15.52 20,10 C20,4.48 15.52,0 10,0 Z'
+                    id='path0_fill' />
+                </g>
+            </g>
+            <path d='M8.68580547,2.95800781 L11.6777344,2.95800781 L11.6777344,6.53302829 L10.9034652,13.1095365 L9.47919237,13.1095365 L8.68580547,6.53302829 L8.68580547,2.95800781 Z M8.75271762,14.2374842 L11.6012633,14.2374842 L11.6012633,17 L8.75271762,17 L8.75271762,14.2374842 Z'
+            id='!' fill='#FFF' />
+        </g>
+    </g>
+</svg>;
+
+export const IconInactiveAlarmSVG = <svg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+    <g id='Page-1' fill='none' fillRule='evenodd'>
+        <g id='test' fillRule='nonzero'>
+            <g id='Shape' fill='green'>
+                <g id='path0_fill-link'>
+                    <path d='M10,0 C4.48,0 0,4.48 0,10 C0,15.52 4.48,20 10,20 C15.52,20 20,15.52 20,10 C20,4.48 15.52,0 10,0 Z'
+                    id='path0_fill' />
+                </g>
+            </g>
+            <g id='Path' transform='translate(3 4)' fill='#FFF'>
+                <g id='path2_fill-link' transform='translate(0 .58)'>
+                    <polygon id='path2_fill' points='5 10.42 0 5.42 1.41 4.01 5 7.59 12.59 0 14 1.42'
+                    />
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>;
+
+export const IconNoAlarmSVG = <svg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+    <g id='Page-1' fill='none' fillRule='evenodd'>
+        <g id='test'>
+            <g id='Shape' fill='#000BFF' fillRule='nonzero'>
+                <g id='path0_fill-link'>
+                    <path d='M10,0 C4.48,0 0,4.48 0,10 C0,15.52 4.48,20 10,20 C15.52,20 20,15.52 20,10 C20,4.48 15.52,0 10,0 Z'
+                    id='path0_fill' />
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>;
