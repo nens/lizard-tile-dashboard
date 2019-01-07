20d093ce4c4e574a1b03a2b04b8ae9fb165e591f
Lirry Pinter <lirry.pinter@nelen-schuurmans.nl>
2019-01-07 14:30:33 +0100

    change tile design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import GridLayout from "./components/GridLayout";
 import FullLayout from "./components/FullLayout";
 import { connect } from "react-redux";
 import { Route, withRouter } from "react-router-dom";
+import logo_Lizard from "./graphics/logo-Lizard.png";
+import { Image } from "react-bootstrap";
 
 import { fetchAlarms } from "./actions";
 import styles from "./App.css";
@@ -17,6 +19,25 @@ class App extends Component {
       <div className={styles.App}>
         <Route exact path="/" component={GridLayout} />
         <Route exact path="/full/:id" component={FullLayout} />
+        <div className="container" id="footer">
+          <div className={styles.logoContainer}>
+            <a href={"https://www.lizard.net/"}>
+              <span className={styles.logoContainerSpan}>
+                Powered by Lizard
+              </span>
+              <span className={styles.logoContainerSpan}> </span>
+              <span className={styles.logoContainerSpan}>
+                <Image
+                  src={logo_Lizard}
+                  className={styles.logo}
+                  responsive
+                  title="Lizard"
+                  style={{ margin: "auto" }}
+                />
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
