e0616bdbc6ea607d613dfd64e3768eaffdf3e8c3
hoanphungt <hoanphung.cpo@gmail.com>
2019-07-23 16:17:56 +0200

    Include aggregation_type as a result from the raster api request

--- src/api_client/valueobjects/RasterStoreDetail.js
+++ src/api_client/valueobjects/RasterStoreDetail.js
@@ -10,7 +10,7 @@ export const RasterStoreDetailDefinition = {
   // Fields from the API
   'url': null,
   'uuid': null,
-
+  'aggregation_type': null,
   'name': null,
   'description': null,
   'organisation': 'Organisation',
