1c01abf02cbb01c8de6a56e8cf250a7f073bf4b5
hoanphungt <hoanphung.cpo@gmail.com>
2019-07-24 09:23:51 +0200

    Re-calculate start and stop value in case of temporal raster

--- src/util/constructRasterAggregatesUrl.js
+++ src/util/constructRasterAggregatesUrl.js
@@ -10,8 +10,10 @@ export default function constructRasterAggregatesUrl(raster, latlng) {
     const shortUuid = raster.uuid.substr(0, 7)
 
     //For temporal rasters, need to include start & stop values
-    const start = new Date(raster.first_value_timestamp);
-    const stop = new Date(raster.last_value_timestamp);
+    //stop value is 1 day ago and start value is 3 days ago from current date
+    const currentDate = new Date();
+    const stop = new Date(currentDate - 1 * 24 * 60 * 60 * 1000);
+    const start = new Date(currentDate - 3 * 24 * 60 * 60 * 1000);
 
     //Formate date & time in the format of YYYY-MM-DDTHH:MM:SS
     const startTime = start.toLocaleDateString().split(/\//).reverse().join('-') + 'T' + start.toLocaleTimeString()
@@ -28,5 +30,6 @@ export default function constructRasterAggregatesUrl(raster, latlng) {
     srs=EPSG:4326&
     styles=dem_nl
     */
+
     return `${layerUrl}agg=${aggType}&geom=POINT+(${latlng.lng}+${latlng.lat})&rasters=${shortUuid}&srs=${srs}&styles=${styles}&start=${startTime}&stop=${stopTime}`
 }
